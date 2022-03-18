a3ce4170bbd692a9eb3cd0d55547f3d4ae1238d9
Leaf Rogers <150157+leafrogers@users.noreply.github.com>
2022-03-18 19:38:12 +0000

    docs: replace generic template text with specific info about this repo

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
 	API_URL: process.env.API_URL || 'https://pokeapi.co/api/v2',
-	APP_FRIENDLY_NAME: 'Template',
+	APP_FRIENDLY_NAME: 'Tic-tac-toe UI',
 	PORT: process.env.PORT || 3000
 };
